6332a0eab4b1d9701699af045ab2dea8075bbad8
Nizamov Maxim <pusher.max@yandex.ru>
2025-07-30 08:03:27 +0300

    code: start point

---
+++ app/camera_controll/start_point.py
@@ -0,0 +1,32 @@
+from overview import Overview
+from carriage import CarriageTracker
+import config
+
+def main():
+    login = config.OVERVIEW["login"]
+    password = config.OVERVIEW["password"]
+    cameras_ips = [config.OVERVIEW[camera]["ip"] for camera in config.OVERVIEW if camera.startswith("camera")]
+    cameras_ports = [config.OVERVIEW[camera]["port"] for camera in config.OVERVIEW if camera.startswith("camera")]
+    
+    overview = Overview(
+        login=login, 
+        password=password, 
+        cameras_ips=cameras_ips, 
+        cameras_ports=cameras_ports, 
+        model_path=config.MODEL_PATH)
+    overview.start()
+
+    carriage_camera = f"rtsp://{login}:{password}@192.168.85.201:554/Streaming/channels/101"
+    tracker = CarriageTracker(
+        #stream_path=config.AIMING,
+        stream_path=carriage_camera,
+        model_path=config.MODEL_PATH,
+        controller_path="/dev/USB0"
+    )
+    tracker.start()
+
+    overview.join()
+    tracker.join()
+
+if __name__ == "__main__":
+    main()

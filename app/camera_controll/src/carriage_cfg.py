6810bfa251d27996dfe0232251068cc11baeee23
Nizamov Maxim <pusher.max@yandex.ru>
2025-08-02 15:15:55 +0300

    small changes carriage controlling: add comments, clear superfluous args

--- app/camera_controll/src/carriage_cfg.py
+++ app/camera_controll/src/carriage_cfg.py
@@ -1,8 +1,11 @@
+# default position setting
 START_X_POSITION = 0
 START_Y_POSITION = 119
 
-MAX_Y_COORD = 145
+# Y (pitch) limits
+MAX_Y_COORD = 150
 MIN_Y_COORD = 50
 
+# X (yaw) limits
 MAX_X_COORD = 40000
-MIN_X_COORD = -4000
+MIN_X_COORD = -40000
